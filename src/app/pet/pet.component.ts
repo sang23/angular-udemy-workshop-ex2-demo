e8be9f53375ccf56201012fcea2d2186bbf47894
sang23 <visrut2538@gmail.com>
2020-07-12 16:32:18 +0700

    add pet

--- src/app/pet/pet.component.ts
+++ src/app/pet/pet.component.ts
@@ -9,9 +9,78 @@ import { ShareService } from '../ShareService';
 })
 export class PetComponent implements OnInit {
 
-  constructor() { }
+  customers: any
+  customer = {
+    name: null,
+    code: null,
+    _id: null
+  }
+
+  pets: any
+
+  pet = {
+    customer_id: null,
+    name: null,
+    remark: null,
+    _id: null
+  }
+
+  constructor(private http: HttpClient, private shareService: ShareService) { }
 
   ngOnInit(): void {
+    this.loadCustomers()
+    this.loadPets()
+  }
+
+  loadCustomers(){
+    this.http.get(this.shareService.serverPath + '/customerAll').subscribe((res: any) =>{
+      this.customers = res
+    })
+  }
+
+  chooseCustomer(customer){
+    this.customer = customer
+  }
+
+  save(){
+    this.pet.customer_id = this.customer._id
+
+    if(this.customer.name != null){
+      this.http.post(this.shareService.serverPath + '/petSave', this.pet).subscribe((res: any) =>{
+        this.loadPets()
+        this.pet.name='';
+        this.pet.remark='';
+        this.customer.name='';
+      })
+    }
+    else{
+      alert("กรุณาเลือกเจ้าของสัตว์เลี้ยง")
+    }
+  }
+
+  loadPets(){
+    this.http.get(this.shareService.serverPath + '/petAll').subscribe((res: any) =>{
+      this.pets = res
+    })
+  }
+
+  petDelete(item){
+    if(confirm("ยืนยันการลบ ?")){
+      var params = {
+        _id: item._id
+      }
+      var path = this.shareService.serverPath +'/petDelete';
+      this.http.post(path, params).subscribe((res: any) => {
+        alert("ลบรายการแล้ว")
+        this.loadPets()
+      })
+    }
+  }
+
+  editPet(item){
+    this.pet = item
+    this.customer = item.customer[0]
+    this.loadPets()
   }
 
 }
